662b63ea49a80a0cf009c4349872e21ad96bfe58
chandan58 <daschandan547@gmail.com>
2023-07-30 22:40:25 +0530

    fifth commit

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { useContext } from "react";
+import {ShopContext} from "./ShopContext";
 
 const CartItems = (props) => {
   const { id, name, image, price, brand } = props.data;
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return <>
-  
+  <section className="cart-item p-5">
+    <div className="container-xxl">
+      <div key={id} className="row">
+        <table>
+          <thead>
+            <th>Product Image</th>
+            <th>Product Details</th>
+            <th>Edit</th>
+            <th>Coupons</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img src={image} alt="" className="img-fluid"/>
+              </td>
+              <td>
+                <p>{brand}</p>
+                <p>{name}</p>
+                <p>{price}</p>
+              </td>
+              <td>
+              <button className="add-btn" onClick={() => {addToCart(id)}}>
+                +
+              </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
   </>;
 };
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,9 @@
 import React,{useContext} from 'react'
 import {PRODUCTS, PRODUCTS1} from "../components/Products"
 import CartItems from '../components/CartItems'
-import shopcontext from '../components/ShopContext'
+import {ShopContext} from '../components/ShopContext'
 
 const Cart = () => {
-  const { id, name, image, price, brand } = props.data;
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return (
     <>
@@ -16,7 +15,7 @@ const Cart = () => {
       })}
         </div> 
     </>
-  )
+  )   
 }
 
 export default Cart
